c63ffe496b366cb5c2c2c4cb5fe3410480788421
David Andersson <david.andersson@zocom.se>
2021-09-06 14:40:59 +0200

    Latest version

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,76 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+	<div class="hello">
+		<h1> Välkommen till Vue demo </h1>
+		<p> {{ exempel }} </p>
+		<button
+			v-show="showButton"
+			v-on:click="toggleShowButton"
+			v-bind:class="hejsanClass"
+			> Hejsan </button>
+		<button v-show="!showButton" v-on:click="toggleShowButton"> Jag är osynlig </button>
+	</div>
+	<div class="">
+		<button v-on:click="disableButton"> Stänga av den andra knappen </button>
+		<button v-bind:disabled="!buttonIsEnabled"> Klicka mig! </button>
+	</div>
+	<div class="">
+		<div v-for="term in terms" v-bind:key="term.id">
+			<strong>{{ term.word }}</strong>:
+			{{ term.explanation}}
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+	name: 'HelloWorld',
+	data: () => ({
+		exempel: 'Detta är exempel på en Data Property.',
+		showButton: true,
+		buttonIsEnabled: true,
+		hejsanClass: 'plain',
+		terms: [
+			{ id: 1, word: 'data property', explanation: 'Variabler i data-objektet' },
+			{ id: 2, word: 'template', explanation: 'Där HTML skrivs' },
+			{ id: 25, word: 'methods', explanation: 'Där vi skriver metoderna som används i HTML/template' }
+		]
+	}),
+	methods: {
+		toggleShowButton() {
+			// console.log('Inside handleClick');
+			this.showButton = !this.showButton
+			this.hejsanClass = 'fancy'
+		},
+		disableButton() {
+			this.buttonIsEnabled = false
+		}
+	}
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.plain {
+	padding: 0.3em 2em;
+	border-radius: 0.2em;
+}
+.fancy {
+	padding: 0.5em 3em;
+	border-radius: 1em;
+	background: hotpink;
+}
 h3 {
-  margin: 40px 0 0;
+	margin: 40px 0 0;
 }
 ul {
-  list-style-type: none;
-  padding: 0;
+	list-style-type: none;
+	padding: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+	display: inline-block;
+	margin: 0 10px;
 }
 a {
-  color: #42b983;
+	color: #42b983;
 }
 </style>
